c8d13740a2f95a17e7ae3105d0f52b18d39d9072
NathJohnston <77366902+NathJohnston@users.noreply.github.com>
2022-09-06 06:36:11 +0800

    Update DaysCountDown.py

--- DaysCountDown.py
+++ DaysCountDown.py
@@ -3,11 +3,14 @@ import datetime
 import pandas as pd
 
 streamlit.title('Holiday Countdown!!')
-streamlit.text('Days left: ')
+#streamlit.text('Days left: ')
 today = datetime.date.today()
 
 #print(today)
 dt = pd.to_datetime(today, format='%Y/%m/%d')
 dt1 = pd.to_datetime('2022/09/15', format='%Y/%m/%d')
 
-daysremaining = (dt1-dt).days
+remaining = (dt1-dt).days
+
+daysleft ='Days left: ' + str(remaining)
+streamlit.text(daysleft)
